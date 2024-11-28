d051cc640bab413396ce4edd5ffd589dc29fcc72
smarkandu <s_markan@hotmail.com>
2024-11-28 17:06:49 -0500

    Update data_collection.py

--- data_collection.py
+++ data_collection.py
@@ -7,6 +7,16 @@ import pandas as pd
 AI_GENERATED = 1
 HUMAN = 0
 
+
+def get_overall_data1():
+    ai_generated_df = convert_text_files_to_df("./data/chatgpt", AI_GENERATED)
+    human_df = convert_text_files_to_df("./data/human", HUMAN)
+
+    df_overall = pd.concat([ai_generated_df, human_df], ignore_index=True)
+
+    return df_overall
+
+
 def get_data1():
     """
     Obtain the given dataset (1 of 2)
@@ -17,21 +27,25 @@ def get_data1():
 
     :return: full_vocab, ai_generated_vocab, human_vocab, df_train, df_test
     """
-    ai_generated_df = convert_text_files_to_df("./data/chatgpt", AI_GENERATED)
-    human_df = convert_text_files_to_df("./data/human", HUMAN)
-
-    df_overall = pd.concat([ai_generated_df, human_df], ignore_index=True)
+    df_overall = get_overall_data1()
 
     df_train, df_test = train_test_split(df_overall, test_size=0.2, random_state=42)
 
     full_vocab = vocab_dictionary(df_train)
     # print(len(full_vocab))
     ai_generated_vocab = vocab_dictionary(df_train[df_train[
-                                                  'label'] == AI_GENERATED])
+                                                       'label'] == AI_GENERATED])
     human_vocab = vocab_dictionary(df_train[df_train[
                                                 'label'] == HUMAN])
 
     return full_vocab, ai_generated_vocab, human_vocab, df_train, df_test
+
+
+def get_overall_data2():
+    df_overall = pd.read_csv('./data2/AI_Human.csv', on_bad_lines='skip', encoding='utf-8')
+    df_overall.columns = ['text', 'label']
+
+    return df_overall
 
 
 def get_data2():
@@ -43,15 +57,14 @@ def get_data2():
     It MUST be downloaded and inserted in the following path (not included due to size)
     :return: full_vocab, ai_generated_vocab, human_vocab, df_train, df_test
     """
-    df_overall = pd.read_csv('./data2/AI_Human.csv', on_bad_lines='skip', encoding='utf-8')
-    df_overall.columns = ['text', 'label']
+    df_overall = get_overall_data2()
 
     df_train, df_test = train_test_split(df_overall, test_size=0.2, random_state=42)
 
     full_vocab = vocab_dictionary(df_train)
     # print(len(full_vocab))
     ai_generated_vocab = vocab_dictionary(df_train[df_train[
-                                                  'label'] == AI_GENERATED])
+                                                       'label'] == AI_GENERATED])
     human_vocab = vocab_dictionary(df_train[df_train[
                                                 'label'] == HUMAN])
 
