4a759fec50bff151435df83d7ded34813cbc24ab
smarkandu <s_markan@hotmail.com>
2024-11-27 21:08:46 -0500

    Update data_collection.py

--- data_collection.py
+++ data_collection.py
@@ -4,25 +4,25 @@ from utils import convert_text_files_to_df
 from utils import vocab_dictionary
 import pandas as pd
 
-CHATGPT = 1
+AI_GENERATED = 1
 HUMAN = 0
 
 def get_data1():
-    chatgpt_df = convert_text_files_to_df("./data/chatgpt", CHATGPT)
+    ai_generated_df = convert_text_files_to_df("./data/ai_generated", AI_GENERATED)
     human_df = convert_text_files_to_df("./data/human", HUMAN)
 
-    df_overall = pd.concat([chatgpt_df, human_df], ignore_index=True)
+    df_overall = pd.concat([ai_generated_df, human_df], ignore_index=True)
 
     df_train, df_test = train_test_split(df_overall, test_size=0.2, random_state=42)
 
     full_vocab = vocab_dictionary(df_train)
     # print(len(full_vocab))
-    chatgpt_vocab = vocab_dictionary(df_train[df_train[
-                                                  'label'] == CHATGPT])
+    ai_generated_vocab = vocab_dictionary(df_train[df_train[
+                                                  'label'] == AI_GENERATED])
     human_vocab = vocab_dictionary(df_train[df_train[
                                                 'label'] == HUMAN])
 
-    return full_vocab, chatgpt_vocab, human_vocab, df_train, df_test
+    return full_vocab, ai_generated_vocab, human_vocab, df_train, df_test
 
 
 def get_data2():
@@ -33,9 +33,9 @@ def get_data2():
 
     full_vocab = vocab_dictionary(df_train)
     # print(len(full_vocab))
-    chatgpt_vocab = vocab_dictionary(df_train[df_train[
-                                                  'label'] == CHATGPT])
+    ai_generated_vocab = vocab_dictionary(df_train[df_train[
+                                                  'label'] == AI_GENERATED])
     human_vocab = vocab_dictionary(df_train[df_train[
                                                 'label'] == HUMAN])
 
-    return full_vocab, chatgpt_vocab, human_vocab, df_train, df_test
+    return full_vocab, ai_generated_vocab, human_vocab, df_train, df_test
