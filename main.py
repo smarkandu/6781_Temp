0d10094dca110fa22ca40e03518ef983177cc7ab
smarkandu <s_markan@hotmail.com>
2024-11-27 21:08:55 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,51 +3,52 @@ import math
 from sklearn.model_selection import train_test_split
 from bag_of_words_a1 import calculate_log_likelihood
 from bag_of_words_a1 import classify_review
-from utils import convert_files_to_csv
+from data_collection import AI_GENERATED, HUMAN
+from utils import convert_text_files_to_df
 from utils import vocab_dictionary
 
 
 if __name__ == '__main__':
-    chatgpt_df = convert_files_to_csv("./data/chatgpt", 0)
-    human_df = convert_files_to_csv("./data/human", 1)
+    ai_generated_df = convert_text_files_to_df("./data/ai_generated", AI_GENERATED)
+    human_df = convert_text_files_to_df("./data/human", HUMAN)
     
-    df_overall = pd.concat([chatgpt_df, human_df], ignore_index=True)
+    df_overall = pd.concat([ai_generated_df, human_df], ignore_index=True)
 
     df_train, df_test = train_test_split(df_overall, test_size=0.2, random_state=42)
 
     full_vocab = vocab_dictionary(df_train)
     # print(len(full_vocab))
-    chatgpt_vocab = vocab_dictionary(df_train[df_train[
-                                                   'label'] == 0])
+    ai_generated_vocab = vocab_dictionary(df_train[df_train[
+                                                   'label'] == AI_GENERATED])
     human_vocab = vocab_dictionary(df_train[df_train[
-                                                   'label'] == 1])
+                                                   'label'] == HUMAN])
     # print(len(human_vocab))
-    # print(len(chatgpt_vocab))
+    # print(len(ai_generated_vocab))
     # print(df_train.head())
 
-    chatgpt_prior = len(df_train[df_train['label'] == 0]) / len(df_train)  # calculate chatgpt prior
-    human_prior = len(df_train[df_train['label'] == 1]) / len(df_train)  # calculate human prior
+    ai_generated_prior = len(df_train[df_train['label'] == AI_GENERATED]) / len(df_train)  # calculate ai_generated prior
+    human_prior = len(df_train[df_train['label'] == HUMAN]) / len(df_train)  # calculate human prior
     # print(human_prior)
-    # print(chatgpt_prior)
+    # print(ai_generated_prior)
 
-    likelihood = calculate_log_likelihood(full_vocab, human_vocab, chatgpt_vocab)
+    likelihood = calculate_log_likelihood(full_vocab, human_vocab, ai_generated_vocab)
 
     assert round(sum([math.exp(likelihood[word]['human']) for word in
                       likelihood])) == 1, "There is probably a bug calculating the human log likelihood"
-    assert round(sum([math.exp(likelihood[word]['chatgpt']) for word in
-                      likelihood])) == 1, "There is probably a bug calculating the chatgpt log likelihood"
+    assert round(sum([math.exp(likelihood[word]['ai_generated']) for word in
+                      likelihood])) == 1, "There is probably a bug calculating the ai_generated log likelihood"
 
     num_correct = 0
     for i in range(0, df_test.shape[0]):
-        predicted_sentiment, sentiment_scores = classify_review(df_test.iloc[i]['text'], likelihood,
-                                                                human_prior, chatgpt_prior)
-        # print(predicted_sentiment)
-        # print(sentiment_scores)
-        if predicted_sentiment == df_test.iloc[i]['label']:
+        predicted_classification, classification_scores = classify_review(df_test.iloc[i]['text'], likelihood,
+                                                                human_prior, ai_generated_prior)
+        # print(predicted_classification)
+        # print(classification_scores)
+        if predicted_classification == df_test.iloc[i]['label']:
             num_correct += 1
 
     print(num_correct / df_test.shape[0])
 
-    # print(predicted_sentiment)
-    # print(sentiment_scores)
+    # print(predicted_classification)
+    # print(classification_scores)
     # print(df_test.iloc[0]['label'])
