8a6f5275ee8618f45a7b15e05e13f981939b3ea1
smarkandu <s_markan@hotmail.com>
2024-11-27 21:09:10 -0500

    Update performance_metrics.py

--- performance_metrics.py
+++ performance_metrics.py
@@ -1,58 +1,15 @@
-def get_Accuracy(TP, TN, FP, FN):
-    return (TP + TN) / (TP + TN + FP + FN)
+from sklearn.metrics import confusion_matrix, classification_report
 
 
-def get_Precision(TP, FP):
-    return TP / (TP + FP)
+def print_all_metrics(target_classifications, predicted_classifications):
+    tn, fp, fn, tp = confusion_matrix(target_classifications, predicted_classifications).ravel()
 
+    # Print metrics
+    print(f"True Positives (TP): {tp}")
+    print(f"True Negatives (TN): {tn}")
+    print(f"False Positives (FP): {fp}")
+    print(f"False Negatives (FN): {fn}")
 
-def get_Recall(TP, FN):
-    return TP / (TP + FN)
-
-
-def get_F1_Score(TP, FP, FN):
-    precision = get_Precision(TP, FP)
-    recall = get_Recall(TP, FN)
-
-    return (2*precision*recall) / (precision + recall)
-
-
-def get_all_metrics(TP, TN, FP, FN):
-    return get_Accuracy(TP, TN, FP, FN), get_Precision(TP, FP), get_Recall(TP, FN), get_F1_Score(TP, FP, FN)
-
-
-def print_all_metrics(TP, TN, FP, FN):
-    accuracy, precision, recall, f1_score = get_all_metrics(TP, TN, FP, FN)
-    print('TP: ', TP)
-    print('TN: ', TN)
-    print('FP: ', FP)
-    print('FN: ', FN)
-    print('\n')
-    print('accuracy: ', accuracy)
-    print('precision: ', precision)
-    print('recall: ', recall)
-    print('f1_score: ', f1_score)
-
-
-def get_basic_performance_metrics(predicted_classifications, target_classifications, positive_classification):
-    TP = 0
-    FP = 0
-    TN = 0
-    FN = 0
-
-    for i in range(0, len(predicted_classifications)):
-        predicted_classification = predicted_classifications[i]
-        target_classification = target_classifications[i]
-
-        if predicted_classification == target_classification:
-            if predicted_classification == positive_classification:
-                TP += 1
-            else:
-                TN += 1
-        else:
-            if predicted_classification == positive_classification:
-                FP += 1
-            else:
-                FN += 1
-
-    return TP, FP, TN, FN
+    # Calculate accuracy, precision, recall, and F1-score
+    report = classification_report(target_classifications, predicted_classifications)
+    print("Classification Report:\n", report)
