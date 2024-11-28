cd8ceea2e96075640b2cd1eb12f8b6217dcd3a99
smarkandu <s_markan@hotmail.com>
2024-11-28 17:35:34 -0500

    Update graphing.py

--- graphing.py
+++ graphing.py
@@ -1,4 +1,3 @@
-import pandas as pd
 import matplotlib.pyplot as plt
 
 
@@ -11,10 +10,18 @@ def get_dataset_histogram(data_df, graph_title):
     fig, ax = plt.subplots()
 
     # Plot label 0 (Human) with blue color
-    ax.hist(data_0['label'], bins=1, alpha=0.7, rwidth=0.8, color='#1f77b4', label='Human')
+    ax.hist(data_0['label'], bins=1, alpha=0.7, rwidth=0.8, color='#1f77b4', label=None)
 
     # Plot label 1 (AI) with orange color
-    ax.hist(data_1['label'], bins=1, alpha=0.7, rwidth=0.8, color='#ff7f0e', label='AI')
+    ax.hist(data_1['label'], bins=1, alpha=0.7, rwidth=0.8, color='#ff7f0e', label=None)
+
+    # Manually add legend labels
+    custom_legend_labels = ['Human', 'AI']
+    custom_colors = ['#1f77b4', '#ff7f0e']
+    custom_handles = [plt.Line2D([0], [0], color=color, lw=4) for color in custom_colors]
+
+    # Add legend
+    ax.legend(custom_handles, custom_legend_labels, title='Label', loc='upper right')
 
     # Set custom x-axis labels to show 'Human' and 'AI'
     ax.set_xticks([0, 1])
@@ -25,9 +32,7 @@ def get_dataset_histogram(data_df, graph_title):
     plt.xlabel('Source')
     plt.ylabel('Frequency')
 
-    # Explicitly set the legend labels
-    plt.legend(title='Label', loc='upper right')
-
     # Show the plot
     plt.show()
+
 
