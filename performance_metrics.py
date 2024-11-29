281cc03e3fce490d64bcec014b8debb84cfb8870
smarkandu <s_markan@hotmail.com>
2024-11-29 01:21:04 -0500

    Update performance_metrics.py

--- performance_metrics.py
+++ performance_metrics.py
@@ -1,22 +1,39 @@
 from sklearn.metrics import confusion_matrix, classification_report
 
 
+def get_Accuracy(TP, TN, FP, FN):
+    return (TP + TN) / (TP + TN + FP + FN)
+
+
+def get_Precision(TP, FP):
+    return TP / (TP + FP)
+
+
+def get_Recall(TP, FN):
+    return TP / (TP + FN)
+
+
+def get_F1_Score(TP, FP, FN):
+    precision = get_Precision(TP, FP)
+    recall = get_Recall(TP, FN)
+
+    return (2*precision*recall) / (precision + recall)
+
+
 def print_all_metrics(target_classifications, predicted_classifications):
-    tn, fp, fn, tp = confusion_matrix(target_classifications, predicted_classifications).ravel()
+    TN, FP, FN, TP = confusion_matrix(target_classifications, predicted_classifications).ravel()
 
     # Print metrics
-    print(f"True Positives (TP): {tp}")
-    print(f"False Positives (FP): {fp}")
-    print(f"True Negatives (TN): {tn}")
-    print(f"False Negatives (FN): {fn}")
+    print(f"True Positives (TP): {TP}")
+    print(f"False Positives (FP): {FP}")
+    print(f"True Negatives (TN): {TN}")
+    print(f"False Negatives (FN): {FN}")
 
     # Calculate accuracy, precision, recall, and F1-score
     report = classification_report(target_classifications, predicted_classifications, digits=4)
-    dict_metrics = classification_report(target_classifications, predicted_classifications, digits=4, output_dict=True)
-    print(dict_metrics.keys())
-    adv_metrics = [dict_metrics["accuracy"], dict_metrics["1.0"]["precision"],
-                   dict_metrics['1.0']["recall"], dict_metrics['1.0']["f1-score"]]
+    adv_metrics = [get_Accuracy(TP, TN, FP, FN), get_Precision(TP, FP),
+                   get_Recall(TP, FN), get_F1_Score(TP, FP, FN)]
 
     print("Classification Report:\n", report)
 
-    return [tp, fp, tn, fn], adv_metrics
+    return [TP, FP, TN, FN], adv_metrics
