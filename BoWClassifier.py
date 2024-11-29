53d8503e27e581855a8bc1a0a0762012041660a0
smarkandu <s_markan@hotmail.com>
2024-11-28 20:25:05 -0500

    Update BoWClassifier.py

--- BoWClassifier.py
+++ BoWClassifier.py
@@ -1,10 +1,7 @@
 import math
 from collections import defaultdict
 
-from sklearn.metrics import confusion_matrix
-from sklearn.model_selection import train_test_split
-
-from data_collection import AI_GENERATED, HUMAN, get_data1, get_data2
+from data_collection import get_data_primary, get_data_secondary
 from performance_metrics import print_all_metrics
 from utils import preprocess_text
 
@@ -151,9 +148,9 @@ def test_model(classifier, human_prior, ai_generated_prior, df_test):
 
     return basic_metrics, adv_metrics
 
-full_vocab, human_vocab, ai_generated_vocab, df_train, df_test = get_data2()
+full_vocab, human_vocab, ai_generated_vocab, df_train, df_test = get_data_primary()
 classifier, ai_generated_prior, human_prior = get_model(full_vocab, human_vocab, ai_generated_vocab, df_train)
 test_model(classifier, human_prior, ai_generated_prior, df_test)
 
-_, _, _, _, df_test1 = get_data1()
+_, _, _, _, df_test1 = get_data_secondary()
 test_model(classifier, human_prior, ai_generated_prior, df_test1)
